4560cf239a26623571a11108343b5b4dd499682e
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-04-15 21:32:20 +0200

    .

--- src/java/M2/LoginServlet.java
+++ src/java/M2/LoginServlet.java
@@ -7,10 +7,12 @@ package M2;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -30,15 +32,31 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
+        HttpSession session = request.getSession(true);
+        
+        Date createTime = new Date (session.getCreationTime());
+        
+        if (session.isNew()) {
+        }
+        
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            String nom=request.getParameter("nom"); 
+            String cognom=request.getParameter("cognom"); 
+            String email=request.getParameter("email"); 
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("<title>Registering with Sessions</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("<ul>");
+            out.println("<li>First Name: " +nom+ "</li>");
+            out.println("<li>Last Name: "+cognom+ "</li>");
+            out.println("<li>Email Address: "+email+ "</li>");
+            out.println("</ul>");
             out.println("</body>");
             out.println("</html>");
         }
