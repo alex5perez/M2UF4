24834d753b67becf76b5426d1a9d6d7cd9103e63
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-04-21 17:13:35 +0200

    .

---
+++ src/java/M2/act2_4.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author alexp
+ */
+public class act2_4 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+  public void doPost (HttpServletRequest request,
+                      HttpServletResponse response)
+      throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    synchronized(session) {
+      @SuppressWarnings("unchecked")
+      List<String> previousItems =
+        (List<String>)session.getAttribute("previousItems");
+      if (previousItems == null) {
+        previousItems = new ArrayList<String>();
+      }
+      String newItem = request.getParameter("newItem");
+      if ((newItem != null) &&
+          (!newItem.trim().equals(""))) {
+        previousItems.add(newItem);
+      }
+      session.setAttribute("previousItems", previousItems);
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String title = "Items Purchased";
+      String docType =
+        "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+        "Transitional//EN\">\n";
+      out.println(docType +
+                  "<HTML>\n" +
+                  "<HEAD><TITLE>" + title + "</TITLE></HEAD>\n" +
+                  "<BODY BGCOLOR=\"#FDF5E6\">\n" +
+                  "<H1>" + title + "</H1>");
+      if (previousItems.size() == 0) {
+        out.println("<I>No items</I>");
+      } else {
+        out.println("<UL>");
+        for(String item: previousItems) {
+          out.println("  <LI>" + item);
+        }
+        out.println("</UL>");
+      }
+      out.println("</BODY></HTML>");
+    }
+  }
+}
