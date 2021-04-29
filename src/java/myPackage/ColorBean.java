15dc488718a471b85a74f6247917aeb0fd526e36
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-04-29 16:18:55 +0200

    .

---
+++ src/java/myPackage/ColorBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myPackage;
+
+/**
+ *
+ * @author alexp
+ */
+public class ColorBean {
+
+    public ColorBean() {
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    public static String foregroundColor = "black";
+    public static String backgroundColor = "white";
+
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    
+    
+}
